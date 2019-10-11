9c7672a3b242463b656808df1269a441e0163aaa
Albert Cheng <lptalbert@gmail.com>
2019-10-11 09:51:07 -0700

    Add backend routes for follow feature

---
+++ app/controllers/api/follows_controller.rb
@@ -0,0 +1,19 @@
+class Api::FollowsController < ApplicationController
+
+  def create
+   
+  end
+
+  def destroy
+   
+  end
+
+  def followers
+
+  end
+
+  def following
+
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   namespace :api, default: {format: :json} do
-    resources :users, only: [:create, :update, :show]
+    resources :users, only: [:create, :update, :show] do
+      get 'followers', to: 'follows#followers'
+      get 'following', to: 'follows#following'
+      post 'follows', to: 'follows#create'
+      delete 'follows', to: 'follows#destroy'
+    end
     resource :session, only: [:create, :destroy]
   end
 
